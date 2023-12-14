9c2c08ba40a47df3a25c240a62a9e9f886404b19
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-14 18:11:54 +0530

    Add Color Constants and Palette Introduce a set of color constants and a color palette in the Flutter app. This commit defines primary, secondary, and background colors, along with a transparent color. Additionally, a theme dialog background color and a vibrant list of colors are declared for consistent theming across the application.

---
+++ lib/utils/values/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const Color bgColor = Color(0xFF000000);
+const Color primaryColor = Color(0xFFFFFFFF);
+const Color secondaryColor = Color(0xFF646464);
+const Color transparentColor = Color(0x00000000);
+const Color themeDialogBgColor = Color(0xFF212121);
+
+final List<Color> colorList = [
+    const Color(0xff65ffa0),
+    const Color(0xffff5252),
+    const Color(0xffd50000),
+    const Color(0xffff4081),
+    const Color(0xffc51162),
+    const Color(0xffe040fb),
+    const Color(0xffaa00ff),
+    const Color(0xff7c4dff),
+    const Color(0xff6200ea),
+    const Color(0xff536dfe),
+    const Color(0xff304ffe),
+    const Color(0xff448aff),
+    const Color(0xff2962ff),
+    const Color(0xff40c4ff),
+    const Color(0xff18ffff),
+    const Color(0xff64ffda),
+    const Color(0xff76ff03),
+    const Color(0xffaeea00),
+    const Color(0xffffff00),
+    const Color(0xffffd740),
+    const Color(0xffff8c40),
+    const Color(0xffff6d00),
+    const Color(0xffff5d2a),
+    const Color(0xffdd2c00),
+];
