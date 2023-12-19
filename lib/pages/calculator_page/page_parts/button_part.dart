bdb00edfe025748806682f9f0baa62c41bcdcf3c
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-19 10:53:44 +0530

    Refactor ButtonPart Widget for Improved Functionality Enhance the "ButtonPart" widget by introducing an expansion feature, allowing for additional mathematical operations and functions. The widget now dynamically adjusts its layout to accommodate both basic and advanced calculator functionalities. This refactor introduces new buttons for trigonometric functions, logarithms, exponentiation, factorial, and mathematical constants like pi and e. Additionally, the layout now supports both expanded and collapsed views, providing a more versatile user interface.

--- lib/pages/calculator_page/page_parts/button_part.dart
+++ lib/pages/calculator_page/page_parts/button_part.dart
@@ -14,7 +14,7 @@ class ButtonPart extends StatefulWidget {
 }
 
 class _ButtonPartState extends State<ButtonPart> {
-  bool isExpanded = false;
+  bool isExpanded = true;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,112 +22,283 @@ class _ButtonPartState extends State<ButtonPart> {
       child: Wrap(
         runSpacing: 5,
         children: [
-          Row(// * : Clear, Backspace, Modulus, Divide
+          if(isExpanded)
+            Row(// * : Ans, log, sin, cos, tan
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomButtonWithText(// * : Ans
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "Ans",
+                  textColor: Theme.of(context).colorScheme.secondary,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : log
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "lg",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : sin
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "sin",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : cos
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "cos",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : tan
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "tan",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ],
+            ),
+          if(isExpanded)
+            Row(// * : Exponent, mod, Open Bracket, Close Bracket, Modulus
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomButtonWrap(// * : Exponent
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  childern1: const Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: Text(
+                      "X",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  childern2: Container(
+                    alignment: Alignment.topLeft,
+                    child: const Text(
+                      "y",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  childern3: Container(),
+                ),
+                CustomButtonWithText(// * : mod
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "|X|",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : Open Bracket
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "(",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : Close Bracket
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: ")",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                CustomButtonWithText(// * : Modulus
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "mod",
+                  textColor: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ],
+            ),
+          Row(// * : Square Root, Clear, Backspace, Percent, Divide
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomButtonWithText(
+              if(isExpanded)
+                SvgButton(// * : Square Root
+                  onTap: () {},
+                  assetPath: "assets/svg/squareRoot.svg",
+                  height: 22,
+                ),
+              CustomButtonWithText(// * : Clear
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "C",
                 textColor: Theme.of(context).colorScheme.secondary,
-                fontSize: 30,
+                fontSize: isExpanded ? 25 : 30,
                 fontWeight: FontWeight.bold,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Backspace
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: Icons.backspace,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Percent
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.percent,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Divide
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.divide,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               )
             ],
           ),
           Row(// * : 7, 8, 9, Multiply
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomButtonWithText(
+              if(isExpanded)
+                CustomButtonWithText(// * : Factorial
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "n!",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                ),
+              CustomButtonWithText(// * : Seven
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "7",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Eight
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "8",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Nine
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "9",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Multiply
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.xmark,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               )
             ],
           ),
-          Row(// * : 4, 5, 6, Subtract
+          Row(// * : Inverse, 4, 5, 6, Subtract
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomButtonWithText(
+              if(isExpanded)
+                CustomButtonWithText(// * : Inverse
+                  onTap: () {},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "1/x",
+                  textColor: Colors.grey,
+                  fontSize: 20,
+                ),
+              CustomButtonWithText(// * : Four
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "4",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Five
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "5",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Six
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "6",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Subtract
                 onTap: (){},
+                btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.minus,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               )
             ],
           ),
-          Row(// * : 1, 2, 3, Add
+          Row(// * : Pi, 1, 2, 3, Add
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomButtonWithText(
+              if(isExpanded)
+                CustomButtonWithIcon(// * : Pi
+                  onTap: () {},
+                  btnSize: isExpanded ? 50 : 65,
+                  icon: const IconData(0X03C0, fontFamily: "Arial Bold"),
+                  iconColor: Colors.grey
+                ),
+              CustomButtonWithText(// * : One
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "1",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Two
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "2",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithText(
+              CustomButtonWithText(// * : Three
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "3",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Add
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.plus,
                 iconColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               )
             ],
           ),
-          Row(// * : Color Palette, 0, Decimal, Equals
+          Row(// * : Expand, e, 0, Decimal, Equals
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Expand
               onTap: () {
                 setState(() => isExpanded = !isExpanded);
               },
@@ -135,22 +306,35 @@ class _ButtonPartState extends State<ButtonPart> {
                 iconColor: Theme.of(context).colorScheme.secondary,
                 iconSize: 25,
               ),
-              CustomButtonWithText(
+              if(isExpanded)
+                CustomButtonWithText(// * : e
+                  onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
+                  buttontext: "e",
+                  textColor: Theme.of(context).primaryColor,
+                  fontSize: isExpanded ? 25 : 30,
+                ),
+              CustomButtonWithText(// * : Zero
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 buttontext: "0",
-                textColor: Theme.of(context).colorScheme.secondary,
+                textColor: Theme.of(context).primaryColor,
+                fontSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Decimal
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: const IconData(46),
-                iconColor: Theme.of(context).colorScheme.secondary,
-                iconSize: 30,
+                iconColor: Theme.of(context).primaryColor,
+                iconSize: isExpanded ? 25 : 30,
               ),
-              CustomButtonWithIcon(
+              CustomButtonWithIcon(// * : Equals
                 onTap: (){},
+                  btnSize: isExpanded ? 50 : 65,
                 icon: FontAwesomeIcons.equals,
                 iconColor: Theme.of(context).primaryColor,
                 bgColor: Theme.of(context).colorScheme.secondary,
+                iconSize: isExpanded ? 25 : 30,
               )
             ],
           )
