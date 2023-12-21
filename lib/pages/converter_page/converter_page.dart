ac9e02a61fef21d9aac3f6c4e6708f43a55c2af3
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-21 19:33:52 +0530

    feat: Create ConverterPage with Conversion Tabs Implement the ConverterPage widget featuring a grid layout with conversion tabs for various units such as Age, Area, BMI, Data, Date, Discount, Length, Mass, Numeral System, Speed, Temperature, Time, and Volume. Each tab includes an icon and label, utilizing custom buttons for a visually appealing user interface.

--- lib/pages/converter_page/converter_page.dart
+++ lib/pages/converter_page/converter_page.dart
@@ -1,10 +1,90 @@
+import 'package:calculator/utils/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class ConverterPage extends StatelessWidget {
   const ConverterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 30,
+        mainAxisSpacing: 30,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 5,
+        vertical: 20
+      ),
+      children: [
+        ConverterTabButton(
+          onTap: () => {},
+          icon: FontAwesomeIcons.cakeCandles,
+          label: "Age",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: FontAwesomeIcons.chartArea,
+          label: "Area",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: LineAwesomeIcons.weight,
+          label: "BMI",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: IconData(0x3386),
+          label: "Data",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: Icons.calendar_month,
+          label: "Date",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: Icons.discount_rounded,
+          label: "Discount",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: FontAwesomeIcons.rulerHorizontal,
+          label: "Length",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: LineAwesomeIcons.hanging_weight,
+          label: "Mass",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: IconData(0x24F5),
+          label: "Numeral System",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: Icons.speed,
+          label: "Speed",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: FontAwesomeIcons.temperatureQuarter,
+          label: "Temprature",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: IconData(0x23F1),
+          label: "Time",
+        ),
+        ConverterTabButton(
+          onTap: () => {},
+          icon: LineAwesomeIcons.cube,
+          label: "Volume",
+        ),
+      ],
+    );
   }
 }
