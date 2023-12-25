9e17a3b9668f69e1581e71c5a52228441864a27f
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-25 12:55:54 +0530

    Refactor AreaPage Widget for Improved Readability Restructured the "AreaPage" widget for enhanced readability and maintainability. Introduced "CustomUnitBoxes" for both the first and second unit boxes to encapsulate their respective logic and improve code organization. Replaced duplicated code with reusable components, reducing redundancy. Improved the naming of variables and functions for better clarity and understanding. Streamlined the handling of dropdown selection and box tapping events. Overall refactoring aimed at making the codebase more modular and comprehensible.

--- lib/pages/converter_page/sub_pages/area_page.dart
+++ lib/pages/converter_page/sub_pages/area_page.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/utils/widgets/custom_button.dart';
 import 'package:calculator/utils/widgets/custom_snackbar.dart';
+import 'package:calculator/utils/widgets/custom_unitboxes.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
@@ -75,182 +76,72 @@ class _AreaPageState extends State<AreaPage> {
     );
   }
 
-  Widget _firstUnitBox(BuildContext context) {
-    return Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        GestureDetector(// * : First Unit Selector
-          onTap: () {
-            setState(() => isFirstDDSelected = true);
-            _showBottomDialog(context, btn: 1);
-          },
-          child: RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: firstUnitBtnDD.endsWith("2")
-                  ? firstUnitBtnDD.substring(0, firstUnitBtnDD.length - 1)
-                  : firstUnitBtnDD,
-                  style: TextStyle(
-                    color: isFirstDDSelected ? Colors.orange : Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                if(firstUnitBtnDD.endsWith("2"))
-                  WidgetSpan(
-                    child: Transform.translate(
-                      offset: const Offset(0, -3),
-                      child: Text(
-                        "2",
-                        style: TextStyle(
-                          color: isFirstDDSelected ? Colors.orange : Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          textBaseline: TextBaseline.alphabetic,
-                        ),
-                      ),
-                    ),
-                  ),
-                WidgetSpan(
-                  alignment: PlaceholderAlignment.middle,
-                  child: Icon(
-                    Icons.arrow_drop_down,
-                    color: isFirstDDSelected ? Colors.orange : Colors.white,
-                  )
-                )
-              ],
+  CustomUnitBoxes _firstUnitBox(BuildContext context) {
+    return CustomUnitBoxes(
+      onTapDropDown: () {
+        setState(() => isFirstDDSelected = true);
+        _showBottomDialog(context, btn: 1);
+      },
+      dropDownDisplay: Row(
+        children: [
+          Text(
+            firstUnitBtnDD,
+            style: TextStyle(
+              color: isFirstDDSelected ? Colors.orange : Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-        Expanded(// * : First Unit Text Box
-          child: GestureDetector(
-            onTap: () {
-              setState(() => isFirstTextSelected = true);
-            },
-            child: Container(
-              color: Colors.transparent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      _areaLogics.firstBoxText,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: isFirstTextSelected ? Colors.orange : Colors.white,
-                      ),
-                    ),
-                  ),
-                  Container(// * : Unit Display
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      unitNameLists[firstSelectedItem],
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
+          Icon(
+            Icons.arrow_drop_down,
+            color: isFirstDDSelected ? Colors.orange : Colors.white,
+          )
+        ],
+      ),
+      onTapBox: () {
+        setState(() => isFirstTextSelected = true);
+      },
+      value: _areaLogics.firstBoxText,
+      isThisBoxSelected: isFirstTextSelected,
+      unitLists: unitNameLists,
+      selectedItem: firstSelectedItem,
     );
   }
 
-  Widget _secondUnitBox(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+
+  CustomUnitBoxes _secondUnitBox(BuildContext context) {
+    return CustomUnitBoxes(
+      onTapDropDown: () {
+        setState(() {
+          isSecondDDSelected = true;
+          isFirstDDSelected = false;
+        });
+        _showBottomDialog(context, btn: 2);
+      },
+      dropDownDisplay: Row(
         children: [
-          GestureDetector(// * : First Unit Selector
-            onTap: () {
-              setState(() => isSecondDDSelected = true);
-              _showBottomDialog(context, btn: 2);
-            },
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: secondUnitBtnDD.endsWith("2")
-                    ? secondUnitBtnDD.substring(0, secondUnitBtnDD.length - 1)
-                    : secondUnitBtnDD,
-                    style: TextStyle(
-                      color: isSecondDDSelected ? Colors.orange : Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  if(secondUnitBtnDD.endsWith("2"))
-                    WidgetSpan(
-                      child: Transform.translate(
-                        offset: const Offset(0, -3),
-                        child: Text(
-                          "2",
-                          style: TextStyle(
-                            color: isSecondDDSelected ? Colors.orange : Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            textBaseline: TextBaseline.alphabetic,
-                          ),
-                        ),
-                      ),
-                    ),
-                  WidgetSpan(
-                    alignment: PlaceholderAlignment.middle,
-                    child: Icon(
-                      Icons.arrow_drop_down,
-                      color: isSecondDDSelected ? Colors.orange : Colors.white,
-                    )
-                  )
-                ],
-              ),
+          Text(
+            secondUnitBtnDD,
+            style: TextStyle(
+              color: isSecondDDSelected ? Colors.orange : Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          Expanded(// * : Second Unit Text Box
-            child: GestureDetector(
-              onTap: () {
-                setState(() => isFirstTextSelected = false);
-              },
-              child: Container(
-                color: Colors.transparent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        _areaLogics.secondBoxText,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: isFirstTextSelected ? Colors.white : Colors.orange,
-                        ),
-                      ),
-                    ),
-                    Container(// * : Unit Display
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        unitNameLists[secondSelectedItem],
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          Icon(
+            Icons.arrow_drop_down,
+            color: isSecondDDSelected ? Colors.orange : Colors.white,
+          )
         ],
-      );
+      ),
+      onTapBox: () {
+        setState(() => isFirstTextSelected = false);
+      },
+      value: _areaLogics.secondBoxText,
+      isThisBoxSelected: !isFirstTextSelected,
+      unitLists: unitNameLists,
+      selectedItem: secondSelectedItem,
+    );
   }
 
   void _showBottomDialog(BuildContext context , {required int btn}) {
