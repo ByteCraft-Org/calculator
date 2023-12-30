6ed767781133bb7ab0f753b3a5a46ea33f29ef1e
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-30 20:45:11 +0530

    Refactor CalculatorPage for Stateful Implementation Convert CalculatorPage from StatelessWidget to StatefulWidget for improved state management. Introduce CalculatorLogics to handle button press logic, and update the UI accordingly. This change enhances the maintainability and flexibility of the calculator page.

--- lib/pages/calculator_page/calculator_page.dart
+++ lib/pages/calculator_page/calculator_page.dart
@@ -1,16 +1,28 @@
+import 'package:calculator/pages/calculator_page/calculator_logics.dart';
 import 'package:calculator/pages/calculator_page/page_parts/button_part.dart';
 import 'package:calculator/pages/calculator_page/page_parts/result_part.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorPage extends StatelessWidget {
+class CalculatorPage extends StatefulWidget {
   const CalculatorPage({super.key});
+
+  @override
+  State<CalculatorPage> createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  final CalculatorLogics logic = CalculatorLogics();
+
+  void _onButtonPressed(String btnText){
+    setState(() => logic.onButtonPressed(btnText));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        const Expanded(// * : Display Segment
-          child: DisplayPart(),
+        Expanded(// * : Display Segment
+          child: DisplayPart(logic: logic),
         ),
         const SizedBox(height: 5,),
         Divider(// * : Divider Line
@@ -18,8 +30,8 @@ class CalculatorPage extends StatelessWidget {
           color: Theme.of(context).primaryColor.withOpacity(0.3),
         ),
         const SizedBox(height: 5,),
-        const SizedBox(// * : Button Segment
-          child: ButtonPart(),
+        SizedBox(// * : Button Segment
+          child: ButtonPart(logic: logic, onButtonPressed: _onButtonPressed),
         ),
       ],
     );
