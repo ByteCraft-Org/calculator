ed4b32310371c5b127c22826ffad9fae22afb39c
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-30 20:43:21 +0530

    Implement logic for calculator button presses Add CalculatorLogics class to handle logic when calculator buttons are pressed. This includes: - Tracking the full expression text and valid expression text - Setting font size for expression text - Handling initial button press when expression is "0" - Switching logic based on button pressed - Adding new text to expression on button press - Handling cases like clear, backspace, operators, numbers, decimal, and equals This lays the foundation for performing actual calculator operations and calculations. More logic can be added here later.

---
+++ lib/pages/calculator_page/calculator_logics.dart
@@ -0,0 +1,29 @@
+class CalculatorLogics{
+  String expressionText = "0"; // * : Full Expression Text written by user.
+  String validExpression = ""; // * : Expression should not contain symbols at end.
+  double expressionFontSize = 40; // * : Set the Font of Expresstion Text.
+
+  void onButtonPressed(String buttonText){
+    if(expressionText == "0") {
+      expressionText = buttonText;
+    } else {
+      switch(buttonText){
+        case "clear":
+        case "backspace":
+        case "%":
+        case "/":
+        case "*":
+        case "-":
+        case "+":
+        case "0":
+        case ".":
+        case "=":
+
+        default:
+          addText(buttonText);
+      }
+    }
+  }
+
+  void addText(String text) => expressionText = expressionText + text;
+}
