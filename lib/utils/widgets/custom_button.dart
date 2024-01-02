dbac47d4981b0f95aa84345b366e72606ab608f3
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2024-01-02 23:31:35 +0530

    Reintroduced CustomButtonWithText and CustomButtonWithIcon in custom_button.dart

--- lib/utils/widgets/custom_button.dart
+++ lib/utils/widgets/custom_button.dart
@@ -1,6 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+class CustomButtonWithText extends StatelessWidget {
+  final Function() onTap;
+  final String buttontext;
+  final Color textColor, bgColor;
+  final FontWeight fontWeight;
+  final double fontSize;
+
+  const CustomButtonWithText({
+    super.key,
+    required this.onTap,
+    required this.buttontext,
+    required this.textColor,
+    this.bgColor = Colors.transparent,
+    this.fontWeight = FontWeight.bold,
+    this.fontSize = 30,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onTap(),
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(bgColor),
+        shadowColor: const MaterialStatePropertyAll(Colors.transparent),
+        surfaceTintColor: const MaterialStatePropertyAll(Colors.transparent),
+        overlayColor: const MaterialStatePropertyAll(Color(0x999E9E9E)),
+        shape: const MaterialStatePropertyAll(LinearBorder()),
+        minimumSize: const MaterialStatePropertyAll(Size(60,60)),
+        padding: const MaterialStatePropertyAll(EdgeInsets.zero)
+      ),
+      child: Text(
+        buttontext,
+        style: TextStyle(
+          color: textColor,
+          fontSize: fontSize,
+          fontWeight: fontWeight
+        ),
+      )
+    );
+  }
+}
+
+class CustomButtonWithIcon extends StatelessWidget {
+  final Function() onTap;
+  final IconData icon;
+  final Color iconColor, bgColor;
+  final double iconSize;
+
+  const CustomButtonWithIcon({
+    super.key,
+    required this.onTap,
+    required this.icon,
+    required this.iconColor,
+    this.bgColor = Colors.transparent,
+    this.iconSize = 25,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onTap(),
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(bgColor),
+        shadowColor: const MaterialStatePropertyAll(Colors.transparent),
+        surfaceTintColor: const MaterialStatePropertyAll(Colors.transparent),
+        overlayColor: const MaterialStatePropertyAll(Color(0x999E9E9E)),
+        shape: const MaterialStatePropertyAll(LinearBorder()),
+        minimumSize: const MaterialStatePropertyAll(Size(60, 60)),
+        padding: const MaterialStatePropertyAll(EdgeInsets.zero)
+      ),
+      child: Icon(
+        icon,
+        color: iconColor,
+        size: iconSize,
+      ),
+    );
+  }
+}
+
 class SvgButton extends StatelessWidget {
   final Function() onTap;
   final String assetPath;
